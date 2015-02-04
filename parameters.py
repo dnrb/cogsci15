012dc8ae2bbc044f60f348474aa3aa774156e468
dnrb <barendbeekhuizen@gmail.com>
2015-02-04 13:32:51 +0100

    second commit

---
+++ parameters.py
@@ -0,0 +1,11 @@
+file_path = 'model_data'
+languages = ['dutch','basque','arabic','nahuatl','quechua',
+             'swahili','thai','indonesian']
+batch_size = 20
+n_simulations = 4
+length_simulation = 1000
+loocv = True
+target_language = 'dutch'
+use_frequency = 'corpus'
+n_components = 6
+n_situations = 71
